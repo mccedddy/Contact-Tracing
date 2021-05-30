eb173ad08995f98d79beea8dc6f0c3e2af7bb092
Daniel John Cedric Macallan <cedricmacallan@gmail.com>
2021-05-30 21:50:00 +0800

    Finished UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,6 +45,15 @@ namespace Contact_Tracing
             this.sexComboBox = new System.Windows.Forms.ComboBox();
             this.btn_Submit = new System.Windows.Forms.Button();
             this.textOtherInformation = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textCovid19
@@ -60,66 +69,59 @@ namespace Contact_Tracing
             // 
             // lastNameTextBox
             // 
-            this.lastNameTextBox.Location = new System.Drawing.Point(12, 196);
+            this.lastNameTextBox.Location = new System.Drawing.Point(11, 223);
             this.lastNameTextBox.Name = "lastNameTextBox";
             this.lastNameTextBox.Size = new System.Drawing.Size(150, 31);
             this.lastNameTextBox.TabIndex = 1;
-            this.lastNameTextBox.Text = "Last Name";
             // 
             // firstNameTextbox
             // 
-            this.firstNameTextbox.Location = new System.Drawing.Point(168, 196);
+            this.firstNameTextbox.Location = new System.Drawing.Point(167, 223);
             this.firstNameTextbox.Name = "firstNameTextbox";
             this.firstNameTextbox.Size = new System.Drawing.Size(327, 31);
             this.firstNameTextbox.TabIndex = 2;
-            this.firstNameTextbox.Text = "First Name";
             // 
             // eMailTextBox
             // 
-            this.eMailTextBox.Location = new System.Drawing.Point(12, 317);
+            this.eMailTextBox.Location = new System.Drawing.Point(11, 372);
             this.eMailTextBox.Name = "eMailTextBox";
-            this.eMailTextBox.Size = new System.Drawing.Size(326, 31);
+            this.eMailTextBox.Size = new System.Drawing.Size(289, 31);
             this.eMailTextBox.TabIndex = 5;
-            this.eMailTextBox.Text = "Email";
             // 
             // phoneNumberTextBox
             // 
-            this.phoneNumberTextBox.Location = new System.Drawing.Point(344, 317);
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(316, 372);
             this.phoneNumberTextBox.Name = "phoneNumberTextBox";
-            this.phoneNumberTextBox.Size = new System.Drawing.Size(150, 31);
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(177, 31);
             this.phoneNumberTextBox.TabIndex = 6;
-            this.phoneNumberTextBox.Text = "Contact Number";
             // 
             // addressTextBox
             // 
-            this.addressTextBox.Location = new System.Drawing.Point(12, 354);
+            this.addressTextBox.Location = new System.Drawing.Point(11, 434);
             this.addressTextBox.Name = "addressTextBox";
             this.addressTextBox.Size = new System.Drawing.Size(483, 31);
             this.addressTextBox.TabIndex = 7;
-            this.addressTextBox.Text = "Address";
             // 
             // temperatureTextBox
             // 
-            this.temperatureTextBox.Location = new System.Drawing.Point(12, 442);
+            this.temperatureTextBox.Location = new System.Drawing.Point(12, 535);
             this.temperatureTextBox.Name = "temperatureTextBox";
             this.temperatureTextBox.Size = new System.Drawing.Size(150, 31);
             this.temperatureTextBox.TabIndex = 8;
-            this.temperatureTextBox.Text = "Temperature";
             // 
             // dateAndTime
             // 
-            this.dateAndTime.Location = new System.Drawing.Point(12, 479);
+            this.dateAndTime.Location = new System.Drawing.Point(170, 535);
             this.dateAndTime.Name = "dateAndTime";
             this.dateAndTime.Size = new System.Drawing.Size(325, 31);
             this.dateAndTime.TabIndex = 9;
             // 
             // ageTextBox
             // 
-            this.ageTextBox.Location = new System.Drawing.Point(12, 233);
+            this.ageTextBox.Location = new System.Drawing.Point(61, 260);
             this.ageTextBox.Name = "ageTextBox";
             this.ageTextBox.Size = new System.Drawing.Size(66, 31);
             this.ageTextBox.TabIndex = 3;
-            this.ageTextBox.Text = "Age";
             // 
             // textContactTracing
             // 
@@ -158,7 +160,7 @@ namespace Contact_Tracing
             // 
             this.textContactInformation.AutoSize = true;
             this.textContactInformation.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textContactInformation.Location = new System.Drawing.Point(12, 286);
+            this.textContactInformation.Location = new System.Drawing.Point(11, 316);
             this.textContactInformation.Name = "textContactInformation";
             this.textContactInformation.Size = new System.Drawing.Size(201, 28);
             this.textContactInformation.TabIndex = 13;
@@ -171,16 +173,15 @@ namespace Contact_Tracing
             this.sexComboBox.Items.AddRange(new object[] {
             "Male",
             "Female"});
-            this.sexComboBox.Location = new System.Drawing.Point(84, 233);
+            this.sexComboBox.Location = new System.Drawing.Point(188, 260);
             this.sexComboBox.Name = "sexComboBox";
             this.sexComboBox.Size = new System.Drawing.Size(129, 33);
             this.sexComboBox.TabIndex = 4;
-            this.sexComboBox.Text = "Sex";
             // 
             // btn_Submit
             // 
             this.btn_Submit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_Submit.Location = new System.Drawing.Point(189, 527);
+            this.btn_Submit.Location = new System.Drawing.Point(188, 592);
             this.btn_Submit.Name = "btn_Submit";
             this.btn_Submit.Size = new System.Drawing.Size(112, 63);
             this.btn_Submit.TabIndex = 14;
@@ -191,18 +192,108 @@ namespace Contact_Tracing
             // 
             this.textOtherInformation.AutoSize = true;
             this.textOtherInformation.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textOtherInformation.Location = new System.Drawing.Point(12, 411);
+            this.textOtherInformation.Location = new System.Drawing.Point(12, 479);
             this.textOtherInformation.Name = "textOtherInformation";
             this.textOtherInformation.Size = new System.Drawing.Size(184, 28);
             this.textOtherInformation.TabIndex = 15;
             this.textOtherInformation.Text = "Other Information";
             this.textOtherInformation.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 25);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Last Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 25);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 25);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Age:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 25);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Sex:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 344);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 25);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Email:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(316, 344);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 25);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Contact Number:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 406);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 25);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Address:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 507);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 25);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Temperature:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(170, 507);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 25);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Date:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 612);
+            this.ClientSize = new System.Drawing.Size(506, 674);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textOtherInformation);
             this.Controls.Add(this.btn_Submit);
             this.Controls.Add(this.sexComboBox);
@@ -221,7 +312,7 @@ namespace Contact_Tracing
             this.Controls.Add(this.textCovid19);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Contact Tracing";
+            this.Text = "Temperature:";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,6 +336,15 @@ namespace Contact_Tracing
         private System.Windows.Forms.ComboBox sexComboBox;
         private System.Windows.Forms.Button btn_Submit;
         private System.Windows.Forms.Label textOtherInformation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
