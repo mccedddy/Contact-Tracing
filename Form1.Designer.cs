ffc73f5e35caea72689c03ad071af603e27a7a30
Daniel John Cedric Macallan <cedricmacallan@gmail.com>
2021-05-31 18:08:11 +0800

    Added input checker, label color changes, added comments

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,7 @@ namespace Contact_Tracing
             this.lastNameTextBox = new System.Windows.Forms.TextBox();
             this.firstNameTextbox = new System.Windows.Forms.TextBox();
             this.eMailTextBox = new System.Windows.Forms.TextBox();
-            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.contactNumberTextBox = new System.Windows.Forms.TextBox();
             this.addressTextBox = new System.Windows.Forms.TextBox();
             this.temperatureTextBox = new System.Windows.Forms.TextBox();
             this.dateAndTime = new System.Windows.Forms.DateTimePicker();
@@ -45,15 +45,15 @@ namespace Contact_Tracing
             this.sexComboBox = new System.Windows.Forms.ComboBox();
             this.btn_Submit = new System.Windows.Forms.Button();
             this.textOtherInformation = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.labelSex = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelContactNumber = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelTemperature = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textCovid19
@@ -88,12 +88,12 @@ namespace Contact_Tracing
             this.eMailTextBox.Size = new System.Drawing.Size(289, 31);
             this.eMailTextBox.TabIndex = 5;
             // 
-            // phoneNumberTextBox
+            // contactNumberTextBox
             // 
-            this.phoneNumberTextBox.Location = new System.Drawing.Point(316, 372);
-            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
-            this.phoneNumberTextBox.Size = new System.Drawing.Size(177, 31);
-            this.phoneNumberTextBox.TabIndex = 6;
+            this.contactNumberTextBox.Location = new System.Drawing.Point(316, 372);
+            this.contactNumberTextBox.Name = "contactNumberTextBox";
+            this.contactNumberTextBox.Size = new System.Drawing.Size(177, 31);
+            this.contactNumberTextBox.TabIndex = 6;
             // 
             // addressTextBox
             // 
@@ -200,101 +200,101 @@ namespace Contact_Tracing
             this.textOtherInformation.Text = "Other Information";
             this.textOtherInformation.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // label1
+            // labelLastName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 195);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 25);
-            this.label1.TabIndex = 16;
-            this.label1.Text = "Last Name:";
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(11, 195);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(99, 25);
+            this.labelLastName.TabIndex = 16;
+            this.labelLastName.Text = "Last Name:";
             // 
-            // label2
+            // labelFirstName
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(167, 195);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 25);
-            this.label2.TabIndex = 17;
-            this.label2.Text = "First Name:";
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(167, 195);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(101, 25);
+            this.labelFirstName.TabIndex = 17;
+            this.labelFirstName.Text = "First Name:";
             // 
-            // label3
+            // labelAge
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 263);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 25);
-            this.label3.TabIndex = 18;
-            this.label3.Text = "Age:";
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(11, 263);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(48, 25);
+            this.labelAge.TabIndex = 18;
+            this.labelAge.Text = "Age:";
             // 
-            // label4
+            // labelSex
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(143, 263);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 25);
-            this.label4.TabIndex = 19;
-            this.label4.Text = "Sex:";
+            this.labelSex.AutoSize = true;
+            this.labelSex.Location = new System.Drawing.Point(143, 263);
+            this.labelSex.Name = "labelSex";
+            this.labelSex.Size = new System.Drawing.Size(43, 25);
+            this.labelSex.TabIndex = 19;
+            this.labelSex.Text = "Sex:";
             // 
-            // label5
+            // labelEmail
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 344);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 25);
-            this.label5.TabIndex = 20;
-            this.label5.Text = "Email:";
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(14, 344);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(58, 25);
+            this.labelEmail.TabIndex = 20;
+            this.labelEmail.Text = "Email:";
             // 
-            // label6
+            // labelContactNumber
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(316, 344);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(147, 25);
-            this.label6.TabIndex = 21;
-            this.label6.Text = "Contact Number:";
+            this.labelContactNumber.AutoSize = true;
+            this.labelContactNumber.Location = new System.Drawing.Point(316, 344);
+            this.labelContactNumber.Name = "labelContactNumber";
+            this.labelContactNumber.Size = new System.Drawing.Size(147, 25);
+            this.labelContactNumber.TabIndex = 21;
+            this.labelContactNumber.Text = "Contact Number:";
             // 
-            // label7
+            // labelAddress
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(11, 406);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(81, 25);
-            this.label7.TabIndex = 22;
-            this.label7.Text = "Address:";
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(11, 406);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(81, 25);
+            this.labelAddress.TabIndex = 22;
+            this.labelAddress.Text = "Address:";
             // 
-            // label8
+            // labelTemperature
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(12, 507);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(114, 25);
-            this.label8.TabIndex = 23;
-            this.label8.Text = "Temperature:";
+            this.labelTemperature.AutoSize = true;
+            this.labelTemperature.Location = new System.Drawing.Point(12, 507);
+            this.labelTemperature.Name = "labelTemperature";
+            this.labelTemperature.Size = new System.Drawing.Size(114, 25);
+            this.labelTemperature.TabIndex = 23;
+            this.labelTemperature.Text = "Temperature:";
             // 
-            // label9
+            // labelDate
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(170, 507);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(53, 25);
-            this.label9.TabIndex = 24;
-            this.label9.Text = "Date:";
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(170, 507);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(53, 25);
+            this.labelDate.TabIndex = 24;
+            this.labelDate.Text = "Date:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(506, 674);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelTemperature);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelContactNumber);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelSex);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.labelLastName);
             this.Controls.Add(this.textOtherInformation);
             this.Controls.Add(this.btn_Submit);
             this.Controls.Add(this.sexComboBox);
@@ -306,7 +306,7 @@ namespace Contact_Tracing
             this.Controls.Add(this.dateAndTime);
             this.Controls.Add(this.temperatureTextBox);
             this.Controls.Add(this.addressTextBox);
-            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.contactNumberTextBox);
             this.Controls.Add(this.eMailTextBox);
             this.Controls.Add(this.firstNameTextbox);
             this.Controls.Add(this.lastNameTextBox);
@@ -325,7 +325,7 @@ namespace Contact_Tracing
         private System.Windows.Forms.TextBox lastNameTextBox;
         private System.Windows.Forms.TextBox firstNameTextbox;
         private System.Windows.Forms.TextBox eMailTextBox;
-        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox contactNumberTextBox;
         private System.Windows.Forms.TextBox addressTextBox;
         private System.Windows.Forms.TextBox temperatureTextBox;
         private System.Windows.Forms.DateTimePicker dateAndTime;
@@ -337,15 +337,15 @@ namespace Contact_Tracing
         private System.Windows.Forms.ComboBox sexComboBox;
         private System.Windows.Forms.Button btn_Submit;
         private System.Windows.Forms.Label textOtherInformation;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.Label labelSex;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelContactNumber;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelTemperature;
+        private System.Windows.Forms.Label labelDate;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,8 +16,18 @@ namespace Contact_Tracing
         {
             InitializeComponent();
         }
+
+        // Notes
+        // CHeck strings
+        // Submit button
+        // Clear textbox after submit
+        // Read Form
+
         // Fields
         int age;
+        long contactNumber;
+        double temperature;
+        bool invalidInput = false;
 
         private void btn_Submit_Click(object sender, EventArgs e)
         {
@@ -29,16 +39,175 @@ namespace Contact_Tracing
             checkContactNumber();
             checkAddress();
             checkTemperature();
+
+            // If invalidInput is true, show invalid input
+            if (invalidInput == true)
+            {
+                MessageBox.Show("Invalid input!");
+                invalidInput = false;
+            }
         }
 
         // Methods
         // Input checker
-        private void checkName() { }
-        private void checkAge() { }
-        private void checkSex() { }
-        private void checkEmail() { }
-        private void checkContactNumber() { }
-        private void checkAddress() { }
-        private void checkTemperature() { }
+        private void checkName() 
+        {
+            // If textbox is not empty, input error is true and change label color to red
+            if (lastNameTextBox.Text != "") 
+            { 
+                labelLastName.ForeColor = Color.Black;
+            }
+
+            // If textbox is empty
+            else 
+            {
+                inputError();
+                labelLastName.ForeColor = Color.Red;
+            }
+
+            // If textbox is not empty, input error is true and change label color to red
+            if (firstNameTextbox.Text != "")
+            {
+                labelFirstName.ForeColor = Color.Black;
+            }
+
+            // If textbox is empty
+            else
+            {
+                inputError();
+                labelFirstName.ForeColor = Color.Red;
+            }
+        }
+        private void checkAge() 
+        {
+            // If age is int
+            try
+            { 
+                age = int.Parse(ageTextBox.Text); 
+
+                // If age is less than 150
+                if (age < 150) 
+                {
+                    labelAge.ForeColor = Color.Black;
+                }
+
+                // If age is more than 150, input error is true and change label color to red
+                else
+                {
+                    inputError();
+                    labelAge.ForeColor = Color.Red;
+                }
+            }
+            // if age is not an int, input error is true and change label color to red
+            catch
+            {
+                inputError();
+                labelAge.ForeColor = Color.Red;
+            }
+        }
+        private void checkSex()
+        { 
+            // If sex is male
+            if (sexComboBox.Text == "Male") 
+            {
+                labelSex.ForeColor = Color.Black;
+            }
+
+            // If sex is female
+            else if (sexComboBox.Text == "Female")
+            {
+                labelSex.ForeColor = Color.Black;
+            }
+
+            // If sex is neither male nor female, input error is true and change label color to red
+            else
+            { 
+                inputError();
+                labelSex.ForeColor = Color.Red;
+            }
+        }
+        private void checkEmail() 
+        { 
+            // If Email contains @ and .
+            if (eMailTextBox.Text.Contains("@") && eMailTextBox.Text.Contains("."))
+            {
+                labelEmail.ForeColor = Color.Black;
+            }
+
+            // If Email does not contain @ and ., input error is true and change label color to red
+            else
+            { 
+                inputError();
+                labelEmail.ForeColor = Color.Red;
+            }
+        }
+        private void checkContactNumber() 
+        {
+            // If contact number is long
+            try
+            {
+                contactNumber = long.Parse(contactNumberTextBox.Text);
+                labelContactNumber.ForeColor = Color.Black;
+            }
+
+            // If contact number is not long, input error is true and change label color to red
+            catch
+            {
+                inputError();
+                labelContactNumber.ForeColor = Color.Red;
+            }
+        }
+        private void checkAddress() 
+        { 
+            // If address is not empty
+            if (addressTextBox.Text != "")
+            {
+                labelAddress.ForeColor = Color.Black;
+            }
+
+            // If address is empty, input error is true and change label color to red
+            else
+            {
+                inputError();
+                labelAddress.ForeColor = Color.Red;
+            }
+        }
+        private void checkTemperature() 
+        {
+            // If temperature is double
+            try
+            {
+                temperature = double.Parse(temperatureTextBox.Text);
+                // If temperature is less than 50 and greater than 10
+                if (temperature < 50 && temperature > 10)
+                {
+                    labelTemperature.ForeColor = Color.Black;
+                }
+
+                // If temperature is more than 50 or less than 10
+                else
+                {
+                    inputError();
+                    labelTemperature.ForeColor = Color.Red;
+                }
+            }
+
+            // If temperature is not double, input error is true and change label color to red
+            catch
+            {
+                inputError();
+                labelTemperature.ForeColor = Color.Red;
+            }
+        }
+
+        // Error
+        private void inputError()
+        {
+            // If invalidInput is false, change to true
+            if (invalidInput == false)
+            { 
+                invalidInput = true; 
+            }
+        }
     }
 }
